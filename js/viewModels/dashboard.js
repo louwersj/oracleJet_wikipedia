0b7faf4679e935547db8d7dbf9677eb871540019
Johan Louwers <louwersj@gmail.com>
2019-01-11 08:52:02 +0000

    added first wikipedia integration js files

--- js/viewModels/dashboard.js
+++ js/viewModels/dashboard.js
@@ -3,75 +3,49 @@
  * The Universal Permissive License (UPL), Version 1.0
  */
 /*
- * Your dashboard ViewModel code goes here
+ * Your incidents ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery'],
- function(oj, ko, $) {
-  
-    function DashboardViewModel() {
+define(['models/mWikipedia', 'ojs/ojcore', 'knockout', 'jquery'],
+ function(wikipedia, oj, ko, $) {
+
+
+
+    function vmWikipedia() {
+
+      this.wikiTitle = ko.observable();
+      this.wikiPageId = ko.observable();
+      this.wikiContent = ko.observable();
+
+       function vmWikipediaParse()
+       {
+         var p = new wikipedia();
+
+         p.wikipediaParse().then(
+             function (info)
+             {
+                self.wikiTitle(info.parse.title);
+                self.wikiPageId(info.parse.pageid);
+                self.wikiContent(info.parse.text["*"]);
+             }).catch(function (reason)
+         {
+             // TODO : enough for now
+             oj.Logger.warn('get appliance info  has failed : %s', reason);
+             console.log(reason)
+         });
+      }
+
+
+
       var self = this;
-      // Below are a subset of the ViewModel methods invoked by the ojModule binding
-      // Please reference the ojModule jsDoc for additional available methods.
 
-      /**
-       * Optional ViewModel method invoked when this ViewModel is about to be
-       * used for the View transition.  The application can put data fetch logic
-       * here that can return a Promise which will delay the handleAttached function
-       * call below until the Promise is resolved.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
-       * the promise is resolved
-       */
-      self.handleActivated = function(info) {
-        // Implement if needed
-      };
-
-      /**
-       * Optional ViewModel method invoked after the View is inserted into the
-       * document DOM.  The application can put logic that requires the DOM being
-       * attached here.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
-       */
-      self.handleAttached = function(info) {
-        // Implement if needed
-      };
-
-
-      /**
-       * Optional ViewModel method invoked after the bindings are applied on this View. 
-       * If the current View is retrieved from cache, the bindings will not be re-applied
-       * and this callback will not be invoked.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       */
-      self.handleBindingsApplied = function(info) {
-        // Implement if needed
-      };
-
-      /*
-       * Optional ViewModel method invoked after the View is removed from the
-       * document DOM.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
-       */
-      self.handleDetached = function(info) {
-        // Implement if needed
-      };
+     vmWikipediaParse()
     }
 
     /*
-     * Returns a constructor for the ViewModel so that the ViewModel is constructed
+     * Returns a constructor for the ViewModel so that the ViewModel is constrcuted
      * each time the view is displayed.  Return an instance of the ViewModel if
      * only one instance of the ViewModel is needed.
      */
-    return new DashboardViewModel();
+    return new vmWikipedia();
   }
 );
