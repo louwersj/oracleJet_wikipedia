ecc1a7b3ee9d015dd041e926ad9c0829c58a7b19
Johan Louwers <louwersj@gmail.com>
2019-01-13 20:35:14 +0000

    added two example cases

--- js/models/mWikipedia.js
+++ js/models/mWikipedia.js
@@ -8,23 +8,18 @@ define(['ojs/ojcore','jquery'],
            }
 
            /**
-            * Parse information in JSON from the wikipedia API.
-            * @returns {promise} - resolve on the following object
-            *   {
-            *     parse {
-            *        title
-            *        pageid
-            *        text {
-            *          *
-            *       }
-            *     }
-            *   }
+            * wikipediaParse will parse information in JSON via the wikipedia
+            * API based upon a pageid. As this function uses the pageid as the
+            * prime way of locating the page you will have to provide this. As
+            * an example you can use 16095 as pageid to get the Jimi Hendrix
+            * page parsed into a JSON format.
+            * !! Do use &callback=? in the URL to prvent CORS issues !!
             */
            wikipedia.prototype.wikipediaParse = function() {
                return(
                        new Promise(function(resolve, reject) {
                            $.ajax( {
-                               'url':'http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?',
+                               'url':'http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&pageid=16095&callback=?',
                                'async': true,
                                'dataType': "json"
                            }).done(
@@ -39,6 +34,34 @@ define(['ojs/ojcore','jquery'],
                        })
                    );
            };
+
+           /**
+            * wikipediaExtract will get an extract of a page in the form of
+            * a JSON file from the wikipedia API. This will require a pageid
+            * as the input. As an example you can use 16099 as pageid to get
+            * the Janet Reno extract into a JSON format.
+            * !! Do use &callback=? in the URL to prvent CORS issues !!
+            */
+           wikipedia.prototype.wikipediaExtract = function() {
+               return(
+                       new Promise(function(resolve, reject) {
+                           $.ajax( {
+                               'url':'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=16099&callback=?',
+                               'async': true,
+                               'dataType': "json"
+                           }).done(
+                               function wikipediaParseDone (data) {
+                                   resolve(data);
+                               }
+                           ).fail(
+                               function wikipediaParseFail (jqxhr, text_status, error_thown) {
+                                   reject(jqxhr.responseJSON ? jqxhr.responseJSON.msg : jqxhr.responseText);
+                               }
+                           );
+                       })
+                   );
+           };
+
 
            return wikipedia;
        });
--- js/viewModels/customers.js
+++ js/viewModels/customers.js
@@ -1,77 +1,39 @@
 /**
- * Copyright (c) 2014, 2017, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
+ * Copyright (c) 2018, Johan Louwers. Licensed under GNU GENERAL PUBLIC LICENSE
  */
-/*
- * Your customer ViewModel code goes here
- */
-define(['ojs/ojcore', 'knockout', 'jquery'],
- function(oj, ko, $) {
-  
-    function CustomerViewModel() {
-      var self = this;
-      // Below are a subset of the ViewModel methods invoked by the ojModule binding
-      // Please reference the ojModule jsDoc for additional available methods.
 
-      /**
-       * Optional ViewModel method invoked when this ViewModel is about to be
-       * used for the View transition.  The application can put data fetch logic
-       * here that can return a Promise which will delay the handleAttached function
-       * call below until the Promise is resolved.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
-       * the promise is resolved
-       */
-      self.handleActivated = function(info) {
-        // Implement if needed
-      };
+define(['models/mWikipedia', 'ojs/ojcore', 'knockout', 'jquery'],
+ function(wikipedia, oj, ko, $) {
 
-      /**
-       * Optional ViewModel method invoked after the View is inserted into the
-       * document DOM.  The application can put logic that requires the DOM being
-       * attached here.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
-       */
-      self.handleAttached = function(info) {
-        // Implement if needed
-      };
-
-
-      /**
-       * Optional ViewModel method invoked after the bindings are applied on this View. 
-       * If the current View is retrieved from cache, the bindings will not be re-applied
-       * and this callback will not be invoked.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       */
-      self.handleBindingsApplied = function(info) {
-        // Implement if needed
-      };
-
-      /*
-       * Optional ViewModel method invoked after the View is removed from the
-       * document DOM.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
-       */
-      self.handleDetached = function(info) {
-        // Implement if needed
-      };
+    function vmWikipedia() {
+      this.wikiTitle = ko.observable();
+      this.wikiPageId = ko.observable();
+      this.wikiContent = ko.observable();
+       function vmWikipediaParse()
+       {
+         var p = new wikipedia();
+         p.wikipediaExtract().then(
+             function (info)
+             {
+               queryResults = (Object.values(info.query.pages)[0])
+               self.wikiTitle(queryResults.title)
+               self.wikiContent(queryResults.extract)
+               self.wikiPageId(queryResults.pageid)
+             }).catch(function (reason)
+         {
+             oj.Logger.warn('get appliance info  has failed : %s', reason);
+          //   console.log(reason)
+         });
+      }
+     var self = this;
+     vmWikipediaParse()
     }
 
     /*
-     * Returns a constructor for the ViewModel so that the ViewModel is constructed
+     * Returns a constructor for the ViewModel so that the ViewModel is constrcuted
      * each time the view is displayed.  Return an instance of the ViewModel if
      * only one instance of the ViewModel is needed.
      */
-    return new CustomerViewModel();
+    return new vmWikipedia();
   }
 );
